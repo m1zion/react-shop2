169010695a0bb5602c9110dbec6d82db459b074d
Misael Lopez <misaelzion@gmail.com>
2023-01-10 00:17:15 -0600

    clase 7 Reacy y css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output:{
@@ -27,13 +28,31 @@ module.exports = {
                         loader:'html-loader',
                     }
                 ]
+            },
+            {
+                //test: /\.s[as]ss$/i, //La del curso esta mal
+                test: /\.(css|scss)$/i,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             }
         ]
     },
     plugins:[
         new HtmlWebpackPlugin({
             template: './public/index.html',
-            filename: 'index.html'
-        })
-    ]
+            filename: './index.html'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer:{
+        historyApiFallback: true,
+        //contentBase: path.join(__dirname,'dist'),
+        //compress:true,
+        //port:3005,
+    }
 }
