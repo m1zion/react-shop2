5a9c16db0d6ab907c72775b90b26c269b55319e2
unknown <misaelzion@gmail.com>
2023-01-12 17:38:45 -0600

    clase 20 useEffect

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,18 +2,18 @@ import React, {useState} from "react";
 import '@styles/ProductItem.scss';
 import iconClose from '@icons/icon_close.png';
 import addButton from '@icons/bt_add_to_cart.svg';
-const ProductItem = () =>{
+const ProductItem = ({product}) =>{
   const [cart,setCart] = useState([]);
   const handleClick = () => {
     setCart([]);
   }
     return (
       <div className="product-card">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
+        <img src={product.images[0]} alt=""/>
         <div className="product-info">
           <div>
-            <p>$120,00</p>
-            <p>Bike</p>
+            <p>${product.price}</p>
+            <p>{product.title}</p>
           </div>
           <figure onClick={handleClick}>
             <img src={addButton} alt=""/>
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         alias:{
             '@components': path.resolve(__dirname, 'src/components/'),
             '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@hooks': path.resolve(__dirname, 'src/hooks/'),
             '@pages': path.resolve(__dirname, 'src/pages/'),
             '@styles': path.resolve(__dirname, 'src/styles/'),
             '@routes': path.resolve(__dirname, 'src/routes/'),
